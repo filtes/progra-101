a9e6c98f84bf7c2601edeec6221fb37d4a720a0a
filtes <joseajimenez@misena.edu.co>
2022-10-02 16:47:51 -0500

    node.js

---
+++ app.ts
@@ -0,0 +1,20 @@
+
+console.log('Hola Dios es Amor Infinito');
+console.log('Ama a Dios Sobre toda las cosas');
+console.log('Dios Tiene El Control De Todo');
+let numberOfLines=0;
+function printLineNumber(){
+    numberOfLines++;
+    numberOfLines++;
+    numberOfLines++;
+    console.log('Linea # ', numberOfLines );
+
+}
+printLineNumber();
+printLineNumber();
+printLineNumber();
+printLineNumber();
+printLineNumber();
+printLineNumber();
+printLineNumber();
+printLineNumber();
